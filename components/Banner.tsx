6a643f6dea3ec08bc7369c827502bc48280787e0
Rishabh Rastogi <rishabh.z3im@gmail.com>
2024-04-24 01:25:33 +0530

    add banner cycle

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,16 +1,47 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import Image from "next/image";
-import React, { useLayoutEffect, useRef, useState } from "react";
+
+const images = [
+  "/assets/banner1.jpg",
+  "/assets/banner2.jpg",
+  "/assets/banner3.jpg",
+];
 
 const Banner = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0); // Index of the current image
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className="flex h-screen p-0 m-0">
-      <Image
-        className="object-cover"
-        fill
-        alt="banner"
-        src="/assets/banner.jpg"
-      />
+    <div className="flex h-screen p-0 m-0 relative">
+      {images.map((image, index) => (
+        <motion.div
+          key={index}
+          className="absolute top-0 left-0 w-full h-full"
+          style={{
+            opacity: index === currentImageIndex ? 1 : 0,
+            zIndex: index === currentImageIndex ? 1 : 0,
+          }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: index === currentImageIndex ? 1 : 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Image
+            className="object-cover"
+            layout="fill"
+            alt="banner"
+            src={image}
+          />
+        </motion.div>
+      ))}
     </div>
   );
 };
