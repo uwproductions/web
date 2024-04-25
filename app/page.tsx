971da0c4b72ca69bed4b0d039ae7159210c2bf08
Rishabh Rastogi <rishabh.z3im@gmail.com>
2024-04-25 23:02:12 +0530

    tiles WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Banner from "@/components/Banner";
+import HomeGrid from "@/components/HomeGrid";
 import Quote from "@/components/Quote";
+import { title } from "process";
 
 export default function Home() {
   return (
     <main className="flex-col bg-white">
       <Banner />
       <Quote />
-      Hi
+      <HomeGrid />
     </main>
   );
 }
---
+++ components/Tile.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import classNames from "classnames";
+
+type TileProps = {
+  src: string;
+  title: string;
+  subtitle: string;
+};
+
+const Tile = ({ src, title, subtitle }: TileProps) => {
+  const [hovered, setHovered] = React.useState(false);
+  return (
+    <div
+      className="flex flex-col items-center relative h-[400px]"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <Image
+        src={src}
+        alt={`image_${src}`}
+        width={545}
+        height={363}
+        className="rounded-2xl"
+      />
+      <motion.div
+        animate={{
+          bottom: hovered ? 60 : 0,
+          transition: {
+            type: "spring", // Type of animation (e.g., "spring", "tween", "keyframes")
+            stiffness: 250, // Adjust stiffness for spring animation
+            damping: 30, // Adjust damping for spring animation
+            ease: "easeInOut", // Easing function for smooth acceleration
+          },
+        }}
+        className={classNames([
+          "absolute",
+          "z-10",
+          "bottom-0",
+          "w-[490px]",
+          "h-[100px]",
+          "bg-white",
+          "rounded-2xl",
+        ])}
+      >
+        <h2 className="text-2xl font-bold">{title}</h2>
+        <p className="text-lg font-light">{subtitle}</p>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Tile;
