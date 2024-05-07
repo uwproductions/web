3820c65a1669f1f461252952223782689af5a022
srivastava-harshal <harshalsrivastava07@gmail.com>
2024-05-07 23:56:43 +0530

    footer css fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,21 +10,21 @@ const Footer = () => {
   const newArr = Array(6).fill(null);
 
   const links = (
-    <div className="flex flex-col md:justify-center font-light gap-2.5 max-w-[30%] md:max-w-full">
+    <div className="flex flex-col md:justify-center font-light gap-1 max-w-[30%] md:max-w-full">
       {navLinks.map((link) => (
         <Link
           key={link.name}
           href={link.href}
-          className={classNames(["text-white text-l md:text-2xl"])}
+          className={classNames(["text-white text-sm md:text-md"])}
         >
-          {link.name}
+          {link.name.toLowerCase()}
         </Link>
       ))}
     </div>
   );
 
   const address = (
-    <div className="flex flex-col gap-2.5 max-w-[70%] md:max-w-full text-white text-l md:text-2xl font-light">
+    <div className="flex flex-col gap-1 max-w-[70%] md:max-w-full text-white text-sm md:text-md font-light">
       {/* <div className="flex flex-col gap-2.5"> */}
       <p>2/14, Sector L, Aashiyana</p>
       <p>Lucknow</p>
@@ -45,9 +45,9 @@ const Footer = () => {
     <div className="py-8 md:py-20 px-7 sm:px-14 md:px-0 bg-black">
       <div
         className="md:flex md:pb-4 pb-8
-      lg:justify-center md:justify-evenly lg:gap-16 border-b"
+      lg:justify-center md:justify-evenly lg:gap-16"
       >
-        <div className="md:max-w-72">
+        <div className="md:max-w-[30rem]">
           <header
             className={classNames([
               "md:text-start text-center mb-8 text-white text-3xl font-semibold text-[40px]",
@@ -56,7 +56,7 @@ const Footer = () => {
           >
             Follow me on Instagram
           </header>
-          <div className="flex flex-wrap justify-evenly">
+          <div className="flex flex-wrap justify-between gap-[1.5rem]">
             {newArr.map((_, index) => (
               <div
                 key={index}
@@ -65,7 +65,7 @@ const Footer = () => {
                   "md:max-w-[calc(33.33%-20px)]",
                   "h-80 md:max-h-20",
                   "relative",
-                  "mb-8 md:mb-4 md:mr-4",
+                  "mb-2",
                   // "md:border md:border-slate-200",
                 ])}
               >
@@ -86,7 +86,18 @@ const Footer = () => {
           {links}
         </div>
       </div>
-      <Logo variant="secondary" />
+      <div className={classNames(["flex", "justify-center"])}>
+        <div className={classNames(["md:min-w-[800px]"])}>
+          <div
+            className={classNames(["flex", "justify-between", "items-center"])}
+          >
+            <Logo variant="secondary" className={classNames(["text-start"])} />
+            <div className={classNames(["text-white", "text-md", "text-end"])}>
+              <p>© Ultrawide Productions</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
