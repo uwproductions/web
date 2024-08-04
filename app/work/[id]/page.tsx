91713950d27040482ec600b4e1e2d3bc786b3bc4
srivastava-harshal <harshalsrivastava07@gmail.com>
2024-08-04 22:48:05 +0530

    projects and images added

--- app/work/[id]/page.tsx
+++ app/work/[id]/page.tsx
@@ -1,5 +1,6 @@
 import projects from "@/app/utils/projects";
 import Banner from "@/components/Banner";
+import Image from "next/image";
 import React from "react";
 
 interface WorkDetailProps {
@@ -12,99 +13,25 @@ const WorkDetail = ({ params: { id } }: WorkDetailProps) => {
   const project = projects.find((project) => project.id === Number(id));
 
   if (!project) {
-    return null;
+    return <p>Project not found.</p>;
   }
 
-  const { title } = project;
+  const { title, images, bannerImages } = project;
+
   return (
     <div>
-      <Banner title={title} src={project.images} />
-      <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-        <div className="grid gap-4">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg object-cover object-center"
-              src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80"
+      <Banner title={title} src={bannerImages} />
+      <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-3 mb-[50px] mx-[20px]">
+        {images.map((image, index) => (
+          <div className="relative w-[350px] md:w-full h-[350px] md:h-[450px]">
+            <Image
+              className="rounded-lg"
+              src={image}
+              layout="fill"
               alt="gallery-photo"
             />
           </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg object-cover object-center "
-              src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
-              alt="gallery-photo"
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg object-cover object-center"
-              src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
-              alt="gallery-photo"
-            />
-          </div>
-        </div>
-        <div className="grid gap-4">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg object-cover object-center"
-              src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
-              alt="gallery-photo"
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg object-cover object-center"
-              src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
-              alt="gallery-photo"
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg object-cover object-center "
-              src="https://docs.material-tailwind.com/img/team-3.jpg"
-              alt="gallery-photo"
-            />
-          </div>
-        </div>
-        <div className="grid gap-4">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg object-cover object-center"
-              src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
-              alt="gallery-photo"
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg object-cover object-center "
-              src="https://docs.material-tailwind.com/img/team-3.jpg"
-              alt="gallery-photo"
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg object-cover object-center"
-              src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
-              alt="gallery-photo"
-            />
-          </div>
-        </div>
-        <div className="grid gap-4">
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg object-cover object-center"
-              src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
-              alt="gallery-photo"
-            />
-          </div>
-          <div>
-            <img
-              className="h-auto max-w-full rounded-lg object-cover object-center"
-              src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
-              alt="gallery-photo"
-            />
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
